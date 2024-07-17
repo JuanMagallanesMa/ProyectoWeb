b3b6ae6f7de4053db4e68b7c2bea7df72493df65
JoffreArriag <153702003+JoffreArriag@users.noreply.github.com>
2024-07-17 09:14:33 -0500

    scriptgastronomia.js

---
+++ Documentos JS/scriptgastronomia.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.body.onkeydown = function(event) {
+        console.log(`Tecla presionada: ${event.key}`);
+        
+        let fontFamily = '';
+        if (event.key === 'a' || event.key === 'A') {
+            fontFamily = 'Arial, sans-serif';
+        } else if (event.key === 's' || event.key === 'S') {
+            fontFamily = '"Courier New", Courier, monospace';
+        } else if (event.key === 'd' || event.key === 'D') {
+            fontFamily = '"Times New Roman", Times, serif';
+        }
+
+        if (fontFamily) {
+            document.body.style.fontFamily = fontFamily;
+        }
+    };
+});
